84d3a1c9ab3ddcf90de8eff5e4f8d039b73830f9
Richard <rra_ncd@hotmail.com>
2018-08-10 16:43:00 -0300

    changed the properties of 'private' to 'readonly'

--- app/ts/models/NegotiationModel.ts
+++ app/ts/models/NegotiationModel.ts
@@ -1,21 +1,9 @@
 export class NegotiationModel {
 
-    constructor(private _date: Date, private _count: number, private _value: number){
-    }
-
-    get date() {
-        return this._date;
-    }
-
-    get count() {
-        return this._count;
-    }
-
-    get value() {
-        return this._value;
+    constructor(readonly date: Date, readonly count: number, readonly value: number){
     }
 
     get volume() {
-        return this._count * this._value;
+        return this.count * this.value;
     }
 }
